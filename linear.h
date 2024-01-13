0c61304edb977eef2dcc7989be26dad90d777251
Georgy Shamteev <georgy.shamteev@gmail.com>
2024-01-13 16:52:29 +0300

    tmp

---
+++ linear.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "eigen-3.4.0/Eigen/Dense"
+#include "eigen-3.4.0/EigenRand/EigenRand"
+
+
+class Linear {
+    ////
+    using Tensor1D = Eigen::Matrix<double, 1, Eigen::Dynamic>;
+    using Tensor2D = Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>;
+    ////
+public:
+    Linear(size_t in_features, size_t out_features, bool bias = true);
+
+    Tensor2D operator()(Tensor2D x);
+
+private:
+    Tensor2D grad_;
+    Tensor2D weight_;
+    Tensor1D bias_;
+};
