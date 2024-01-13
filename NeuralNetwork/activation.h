b4a524e9bced5114f50f129359791f92a4f061ba
Georgy Shamteev <georgy.shamteev@gmail.com>
2024-01-13 15:45:55 +0300

    correct formulas in Linear (probably) and activatioan file created

--- NeuralNetwork/activation.h
+++ NeuralNetwork/activation.h
@@ -2,26 +2,18 @@
 #include "../EigenRand/EigenRand/EigenRand"
 
 namespace nn {
-class Linear {
+class ActivationFunction {
     ////
     using Tensor1D = Eigen::Matrix<double, 1, Eigen::Dynamic>;
     using Tensor2D = Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>;
     using Tensor3D = std::vector<Tensor2D>;
     ////
 public:
-    Linear() = delete;
-    Linear(size_t in_features, size_t out_features, bool bias = true);
     Tensor2D operator()(Tensor2D x);
     void Update(Tensor2D u);
     Tensor2D RethrowGradient(Tensor2D u);
 
 private:
-    Tensor2D input_seq_;
-    Tensor2D output_seq_;
-    Tensor2D weight_grad_;
-    Tensor1D bias_grad_;
-    Tensor2D weight_;
-    Tensor1D bias_;
-    inline static Eigen::Rand::Vmt19937_64 urng{42};
+    Tensor3D d_sigma_;
 };
 }  // namespace nn
--- NeuralNetwork/linear.cpp
+++ NeuralNetwork/linear.cpp
@@ -1,16 +1,20 @@
 #include "linear.h"
 
+namespace nn {
 Linear::Linear(size_t in_features, size_t out_features, bool bias)
     : weight_(in_features, out_features), bias_(out_features) {
+    //// TODO: insert asserts
+
     double k = 1 / in_features;
     double sqrt_k = std::sqrt(k);
     Eigen::Rand::UniformRealGen gen(-sqrt_k, sqrt_k);
+    bias_.setZero();
     if (bias) {
         bias_ = gen.generate<Tensor1D>(1, out_features, urng);
     }
     weight_ = gen.generate<Tensor2D>(in_features, out_features, urng);
 
-    //// TODO initialize other class fields
+    //// TODO: initialize other class fields
 }
 
 Linear::Tensor2D Linear::operator()(Linear::Tensor2D x) {
@@ -23,8 +27,12 @@ void Linear::Update(Linear::Tensor2D u) {
      * Thus vector u coming for update is actually vector u` = d sigma * u,
      * where u is the gradient vector that was fed to non-linear layer
      */
-    weight_grad_ = u * input_seq_;
-    bias_grad_ = u;
+    //// TODO: insert asserts
+
+    for (size_t i = 0; i < u.cols(); ++i) {
+        weight_grad_ += u.col(i) * input_seq_.row(i);
+        bias_grad_ += u.col(i);
+    }
     weight_ -= weight_grad_;
     bias_ -= bias_grad_;
     weight_grad_.setZero();
@@ -32,5 +40,6 @@ void Linear::Update(Linear::Tensor2D u) {
 }
 Linear::Tensor2D Linear::RethrowGradient(Linear::Tensor2D u) {
     //// Needs to be done before weight updating or not?
-    return weight_ * u;
+    return u.transpose() * weight_;
 }
+}  // namespace nn
