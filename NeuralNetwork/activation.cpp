dd36f7ca209372ca2c39bfb170b29137027d80e2
Georgy Shamteev <georgy.shamteev@gmail.com>
2024-04-13 17:38:49 +0300

    fixed problems with act funcs, numerical stability and covered with tests

--- NeuralNetwork/activation.cpp
+++ NeuralNetwork/activation.cpp
@@ -6,6 +6,7 @@ ActivationFunction ActivationFunction::ReLU() {
     auto sigma = [](const Tensor2D& m) -> Tensor2D {
         return m.unaryExpr([](double x) -> double { return x > 0 ? x : 0; });
     };
+
     auto dsigma = [](const Tensor1D& m) -> Tensor2D {
         return m.unaryExpr([](double x) -> double { return x > 0 ? 1 : 0; }).asDiagonal();
     };
@@ -17,6 +18,7 @@ ActivationFunction ActivationFunction::Sigmoid() {
     auto sigma = [](const Tensor2D& m) -> Tensor2D {
         return m.unaryExpr([](double x) -> double { return 1 / (1 + std::exp(-x)); });
     };
+
     auto dsigma = [](const Tensor1D& m) -> Tensor2D {
         return m
             .unaryExpr([](double x) -> double {
@@ -32,6 +34,7 @@ ActivationFunction ActivationFunction::Tanh() {
     auto tanh = [](double x) -> double {
         return (std::exp(x) - std::exp(-x)) / (std::exp(x) + std::exp(-x));
     };
+
     auto sigma = [tanh](const Tensor2D& m) -> Tensor2D { return m.unaryExpr(tanh); };
 
     auto dsigma = [tanh](const Tensor1D& m) -> Tensor2D {
@@ -42,29 +45,63 @@ ActivationFunction ActivationFunction::Tanh() {
             })
             .asDiagonal();
     };
+
     return ActivationFunction(sigma, dsigma);
 }
 
 ActivationFunction ActivationFunction::Softmax() {
     auto sigma = [](const Tensor2D& m) -> Tensor2D {
-        return m.unaryExpr([](double x) -> double { return 1 / (1 + std::exp(-x)); });
+        Tensor2D ret = m;
+        for (Index i = 0; i < m.rows(); ++i) {
+            double c = m.row(i).maxCoeff();
+            double y = 0;
+            for (Index j = 0; j < m.cols(); ++j) {
+                y += std::exp(m(i, j) - c);
+            }
+            y = c + std::log(y);
+            for (Index j = 0; j < m.cols(); ++j) {
+                ret(i, j) = std::exp(m(i, j) - y);
+            }
+        }
+        return ret;
     };
 
     auto dsigma = [](const Tensor1D& m) -> Tensor2D {
-        return m.unaryExpr([](double x) -> double { return x > 0 ? 1 : 0; }).asDiagonal();
+        Tensor2D ret(m.size(), m.size());
+        double c = m.maxCoeff();
+        double y = 0;
+        for (Index j = 0; j < m.cols(); ++j) {
+            y += std::exp(m(0, j) - c);
+        }
+        y = c + std::log(y);
+
+        for (Index i = 0; i < m.cols(); ++i) {
+            for (Index j = 0; j < m.cols(); ++j) {
+                double si = std::exp(m(0, i) - y);
+                double sj = std::exp(m(0, j) - y);
+
+                if (i == j) {
+                    ret(i, j) = si * (1 - sj);
+                } else {
+                    ret(i, j) = -si * sj;
+                }
+            }
+        }
+        return ret;
     };
+
     return ActivationFunction(sigma, dsigma);
 }
 
 NeuralDefines::Tensor2D ActivationFunction::operator()(const NeuralDefines::Tensor2D& x) {
     input_ = x;
-    return sigma_(x);
+    return sigma_(x).eval();
 }
 
 NeuralDefines::Tensor2D ActivationFunction::Update(const NeuralDefines::Tensor2D& u) {
     Tensor2D ret(u.cols(), u.rows());
     for (Index i = 0; i < u.rows(); ++i) {
-        ret.col(i) = dsigma_(input_.row(i)) * (u.row(i).transpose());
+        ret.col(i) = dsigma_(input_.row(i)).eval() * (u.row(i).transpose()).eval();
     }
     return ret;
 }
